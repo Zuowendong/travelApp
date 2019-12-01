0fdd442a1eac04c2e60d9a11b606ef87c98092d3
zuowendong <17326195691@163.com>
2019-12-01 22:14:30 +0800

    project init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>hello</div>
+    <router-link to="/list">跳转</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+
+</style>
